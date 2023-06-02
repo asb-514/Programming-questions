2581b5eaf505778eda15b129c49890155ae2e0f9
Shashank Annangi <shashank@Shashanks-MacBook-Pro.local>
2023-06-02 19:21:08 +0530

    formated

--- Maximum_width.cpp
+++ Maximum_width.cpp
@@ -1,9 +1,9 @@
-//  https://codeforces.com/problemset/problem/1492/C
+// https://codeforces.com/problemset/problem/1492/C
 // include " Maximum width.h"
 
 #ifdef ONPC
 #define _GLIBCXX_DEBUG
-//#include "bits/stdc++.h"
+// #include "bits/stdc++.h"
 #else
 #include <bits/stdc++.h>
 #endif
@@ -24,48 +24,59 @@ typedef long double ld;
 #define d(...)
 #endif
 
-int solve() {
+int solve()
+{
     ll n;
     if (!(cin >> n)) {
         return 1;
     }
-    ll m; cin >> m;
-    string a; cin >> a;
-    string b; cin >> b;
+    ll m;
+    cin >> m;
+    string a;
+    cin >> a;
+    string b;
+    cin >> b;
     vector<ll> l(m);
     vector<ll> r(m);
     ll j = 0;
     for (ll i = 0; i < m; i++) {
-        while(j < n && a[j] != b[i]) j++;
+        while (j < n && a[j] != b[i])
+            j++;
         l[i] = j;
         j++;
     }
-    j = n-1;
-    for (ll i = m-1 ; i > -1; i--) {
-        while (j>-1 && a[j] != b[i]) {
+    j = n - 1;
+    for (ll i = m - 1; i > -1; i--) {
+        while (j > -1 && a[j] != b[i]) {
             j--;
         }
         r[i] = j;
-        j-- ;
+        j--;
     }
     ll ans = 0;
-    for (ll i = 0; i < m-1; i++) {
-        ans = max(ans , r[i + 1] - l[i]);
+    for (ll i = 0; i < m - 1; i++) {
+        ans = max(ans, r[i + 1] - l[i]);
     }
     cout << ans << endl;
     return 0;
 }
 
-int32_t main() {
-    //sieve(2e5+10);
-    ios::sync_with_stdio(0); cin.tie(0); int TET = 1e9;
+int32_t main()
+{
+    // sieve(2e5+10);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int TET = 1e9;
     for (int i = 1; i <= TET; i++) {
-        if (solve()) {break;}
+        if (solve()) {
+            break;
+        }
 #ifdef ONPC
         cout << "__________________________" << endl;
 #endif
     }
 #ifdef ONPC
-    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+    cerr << endl
+         << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
 #endif
 }
